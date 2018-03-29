bf0013d4efd76ebc3b27d65ceae6933ebbb43d83
Marc Ohlmann <marc.ohlmann@univ-grenoble-alpes.fr>
2018-03-29 16:54:00 +0200

    Update dis.beta.R

--- R/dis.beta.R
+++ R/dis.beta.R
@@ -19,17 +19,21 @@ dis.beta <- function(g.list,groups,eta=1,framework=c('RLC','Tu'),type=c('P','L',
       }
     }
     if(type=='L'){
-      n.groups=nrow(metaweb.array$P.mat)
+    n.groups=nrow(metaweb.array$P.mat)
       meta.links <- aperm(metaweb.array$L.array,c(2,1,3))  
       dim(meta.links) <- c(n.groups*n.groups,ncol(metaweb.array$P.mat)) 
       colnames(meta.links) <- colnames(metaweb.array$P.mat) 
-      if(sum(rowSums(meta.links)>0)==ncol(meta.links)){
+      if(sum(rowSums(meta.links)>0)<nrow(meta.links)){
         meta.links=meta.links[-which(rowSums(meta.links)==0),]
       }
       spxp=meta.links
       for (i in 2:N) {
+        print(i)
         for (j in 1:(i-1)) {
           spxp.dummy <- spxp[,c(i,j)]
+          if(sum(rowSums(spxp.dummy)>0)<nrow(spxp.dummy)){
+            spxp.dummy=spxp.dummy[-which(rowSums(spxp.dummy)==0),]
+          }
           spxp.dummy= spxp.dummy/sum(spxp.dummy)
           spxp.meta=metacommunity(spxp.dummy)
           dis[i, j] <- dis[j, i] <- as.numeric(norm_meta_beta(spxp.meta,qs = eta)[7])
