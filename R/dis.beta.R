770176759472e2514f3697c081b209e417f8507b
Marc Ohlmann <marc.ohlmann@univ-grenoble-alpes.fr>
2018-04-25 15:35:07 +0200

    Update dis.beta.R

--- R/dis.beta.R
+++ R/dis.beta.R
@@ -1,4 +1,8 @@
 dis.beta <- function(g.list,groups=NULL,eta=1,framework=c('RLC','Tu'),type=c('P','L','Pi')){
+  
+  if(sum(unlist(lapply(lapply(g.list,FUN = function(x) V(x)$name),is.null)))>0){
+    stop('nodes must have names (use V(g)$name)')
+  }
    if(is.null(groups)){
     groups=unique(unlist(lapply(g.list,FUN = function(g) V(g)$name)))
     names(groups)=groups
@@ -7,10 +11,10 @@ dis.beta <- function(g.list,groups=NULL,eta=1,framework=c('RLC','Tu'),type=c('P'
   if(is.null(names(groups))){
     stop("groups must have names (names(groups) is NULL)")
   }
- 
-     if(is.null(names(groups))){
-        stop("groups must have names (names(groups) is NULL)")
-    }
+  if(prod(names(groups) %in% unique(unlist(lapply(g.list,FUN = function(g) V(g)$name))))*prod(unique(unlist(lapply(g.list,FUN = function(g) V(g)$name))) %in% names(groups))!=1){
+    stop("the names of groups vector do not match to the names of the metaweb")
+  }
+
   metaweb.array <- metaweb.params(g.list,groups)
   N <- ncol( metaweb.array$P.mat)
   dis <- matrix(NA, N, N)
@@ -18,17 +22,21 @@ dis.beta <- function(g.list,groups=NULL,eta=1,framework=c('RLC','Tu'),type=c('P'
   if(framework=='RLC'){
     if(type=='P'){
       spxp=metaweb.array$P.mat
+      pb <- txtProgressBar(min = 0, max = N*(N-1)/2, style = 3)
+      comp=0
       for (i in 2:N) {
         for (j in 1:(i-1)) {
+          comp=comp+1
           spxp.dummy <- spxp[,c(i,j)]
           spxp.dummy= spxp.dummy/sum(spxp.dummy)
           spxp.meta=metacommunity(spxp.dummy)
           dis[i, j] <- dis[j, i] <- as.numeric(norm_meta_beta(spxp.meta,qs = eta)[7])
+          setTxtProgressBar(pb,comp)
         }
       }
     }
     if(type=='L'){
-    n.groups=nrow(metaweb.array$P.mat)
+     n.groups=nrow(metaweb.array$P.mat)
       meta.links <- aperm(metaweb.array$L.array,c(2,1,3))  
       dim(meta.links) <- c(n.groups*n.groups,ncol(metaweb.array$P.mat)) 
       colnames(meta.links) <- colnames(metaweb.array$P.mat) 
@@ -36,8 +44,11 @@ dis.beta <- function(g.list,groups=NULL,eta=1,framework=c('RLC','Tu'),type=c('P'
         meta.links=meta.links[-which(rowSums(meta.links)==0),]
       }
       spxp=meta.links
+      pb <- txtProgressBar(min = 0, max = N*(N-1)/2, style = 3)
+      comp=0
       for (i in 2:N) {
         for (j in 1:(i-1)) {
+          comp=comp+1
           spxp.dummy <- spxp[,c(i,j)]
           if(sum(rowSums(spxp.dummy)>0)<nrow(spxp.dummy)){
             spxp.dummy=spxp.dummy[-which(rowSums(spxp.dummy)==0),]
@@ -45,6 +56,7 @@ dis.beta <- function(g.list,groups=NULL,eta=1,framework=c('RLC','Tu'),type=c('P'
           spxp.dummy= spxp.dummy/sum(spxp.dummy)
           spxp.meta=metacommunity(spxp.dummy)
           dis[i, j] <- dis[j, i] <- as.numeric(norm_meta_beta(spxp.meta,qs = eta)[7])
+          setTxtProgressBar(pb, comp)
         }
       }
     }
@@ -54,14 +66,18 @@ dis.beta <- function(g.list,groups=NULL,eta=1,framework=c('RLC','Tu'),type=c('P'
       dim(meta.Pi) <- c(n.groups*n.groups,ncol(metaweb.array$P.mat)) 
       colnames(meta.Pi) <- colnames(metaweb.array$P.mat) 
       spxp=meta.Pi
+      pb <- txtProgressBar(min = 0, max = N*(N-1)/2, style = 3)
+      comp=0
       for (i in 2:N) {
         for (j in 1:(i-1)) {
+          comp=comp+1
           spxp.dummy <- spxp[,c(i,j)]
           if(length(c(which(is.na(rowSums( spxp.dummy))),which(rowSums(spxp.dummy)==0)))>0){
             spxp.dummy=spxp.dummy[-c(which(is.na(rowSums( spxp.dummy))),which(rowSums(spxp.dummy)==0)),]}
           spxp.dummy= spxp.dummy/sum(spxp.dummy)
           spxp.meta=metacommunity(spxp.dummy)
           dis[i, j] <- dis[j, i] <- as.numeric(norm_meta_beta(spxp.meta,qs = eta)[7])
+          setTxtProgressBar(pb, comp)
         }
       }
     }
@@ -69,11 +85,15 @@ dis.beta <- function(g.list,groups=NULL,eta=1,framework=c('RLC','Tu'),type=c('P'
   if(framework=='Tu'){
     if(type=='P'){
       spxp=t(metaweb.array$P.mat)
+      pb <- txtProgressBar(min = 0, max = N*(N-1)/2, style = 3)
+      comp=0
       for (i in 2:N) {
         for (j in 1:(i-1)) {
+          comp=comp+1
           spxp.dummy <- spxp[c(i,j), ]
           res <- abgDecompQ(as.matrix(spxp.dummy), q = eta, check = FALSE)$Beta
           dis[i, j] <- dis[j, i] <- res
+          setTxtProgressBar(pb, comp)
         }
       }
     }
@@ -86,8 +106,11 @@ dis.beta <- function(g.list,groups=NULL,eta=1,framework=c('RLC','Tu'),type=c('P'
         meta.links=meta.links[-which(rowSums(meta.links)==0),]
       }
       spxp=t(meta.links)
+      pb <- txtProgressBar(min = 0, max = N*(N-1)/2, style = 3)
+      comp=0
       for (i in 2:N) {
         for (j in 1:(i-1)) {
+          comp=comp+1
          spxp.dummy <- spxp[,c(i,j)]
           if(sum(rowSums(spxp.dummy)>0)<nrow(spxp.dummy)){
             spxp.dummy=spxp.dummy[-which(rowSums(spxp.dummy)==0),]
@@ -95,6 +118,7 @@ dis.beta <- function(g.list,groups=NULL,eta=1,framework=c('RLC','Tu'),type=c('P'
           spxp.dummy= spxp.dummy/sum(spxp.dummy)
           res <- abgDecompQ(as.matrix(spxp.dummy), q = eta, check = FALSE)$Beta
           dis[i, j] <- dis[j, i] <- res
+          setTxtProgressBar(pb, comp)
         }
       }
     }
@@ -107,14 +131,18 @@ dis.beta <- function(g.list,groups=NULL,eta=1,framework=c('RLC','Tu'),type=c('P'
         meta.Pi=meta.Pi[-c(which(is.na(rowSums(meta.Pi))),which(rowSums(meta.Pi)==0)),]
       }
       spxp=t(meta.Pi)
+      pb <- txtProgressBar(min = 0, max = N*(N-1)/2, style = 3)
+      comp=0
       for (i in 2:N) {
         for (j in 1:(i-1)) {
+          comp=comp+1
         spxp.dummy <- spxp[c(i,j), ]
         if(length(c(which(is.na(rowSums( spxp.dummy))),which(rowSums(spxp.dummy)==0)))>0){
             spxp.dummy=spxp.dummy[-c(which(is.na(rowSums( spxp.dummy))),which(rowSums(spxp.dummy)==0)),]}
          spxp.dummy= spxp.dummy/sum(spxp.dummy)
          res <- abgDecompQ(as.matrix(spxp.dummy), q = eta, check = FALSE)$Beta
          dis[i, j] <- dis[j, i] <- res
+         setTxtProgressBar(pb, comp)
         }
       }
     }
