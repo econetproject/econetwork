ea2ddd8bff7d14b1f91f26396c5744ac52a153ca
Marc Ohlmann <marc.ohlmann@univ-grenoble-alpes.fr>
2018-04-26 11:30:44 +0200

    Update get.metaweb.R

--- R/get.metaweb.R
+++ R/get.metaweb.R
@@ -1,4 +1,4 @@
-get.metaweb <- function(g.list){
+get.metaweb <- function(g.list){#get the metaweb of a list of graph (as the union on a set of graph)
   g.metaweb <- g.list[[1]]
   for(i in 2:length(g.list)){
     g.metaweb <- g.metaweb %u% g.list[[i]]
